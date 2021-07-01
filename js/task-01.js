a7af7cd14b3e98582df0221d16eb105f1b8f08eb
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-07-01 16:58:20 +0300

    del

--- js/task-01.js
+++
@@ -1,6 +0,0 @@
-const categories = document.querySelectorAll('.item');
-
-const category = [...categories].map(el => {
-    console.log('Категория:', el.firstElementChild.textContent);
-    console.log('Количество элементов:', el.lastElementChild.querySelectorAll('li').length);
-});
